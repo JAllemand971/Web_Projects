d2696255815cf57580105a2dde6bdab80435a2e7
Jeremy Allemand <jeremyallemand971@gmail.com>
2024-11-29 00:39:17 -0500

    Update scripttp3.js

--- firstjavascript/script/scripttp3.js
+++ firstjavascript/script/scripttp3.js
@@ -1,81 +1,82 @@
-/* Jeremy Allemand, student ID: 20312390, IFT-1144-A-A24, TP3 */
+/* Jeremy Allemand, matricule: 20312390, IFT-1144-A-A24, TP3 */
 
 /*
 -------------------------------------------------------
-Title: Function 1 - Convert
-Purpose: Convert a number into various formats
+Title: Function 1 - convert	 
+Purpose: Convert a number to various formats
 -------------------------------------------------------
 */
 function convert(event) {
-    let number = parseFloat(document.getElementById("number").value); // declare a float from the input
+    let number = parseFloat(document.getElementById("nombre").value); // declare a float from the input
 
-    if (event.target.id === "centimeter") {
-        let value1 = (number * 2.5).toFixed(2); // conversion from inch to centimeter with 2 decimal places
-        document.getElementById("result").innerHTML = "inches ===> " + value1;
+    if (event.target.id === "centimetre"){
+        let value1 =  (number * 2.5).toFixed(2); // convert inches to centimeters with 2 decimals
+        document.getElementById("resultat").innerHTML = "inches ===> " + value1;
     }
 
-    else if (event.target.id === "inch") {
-        let value2 = (number / 2.5).toFixed(2); // conversion from centimeter to inch with 2 decimal places
-        document.getElementById("result").innerHTML = "cm ===> " + value2;
+    else if(event.target.id === "pouce") {
+        let value2 = (number / 2.5).toFixed(2); // convert centimeters to inches with 2 decimals
+        document.getElementById("resultat").innerHTML = "cm ===> " + value2;
     }
 
-    else if (event.target.id === "fahrenheit") {
-        let value3 = (number * 9 / 5 + 32).toFixed(2); // conversion from Celsius to Fahrenheit with 2 decimal places
-        document.getElementById("result").innerHTML = "Celsius ===> " + value3;
+    else if(event.target.id === "fahrenheit"){
+        let value3 = (number * 9 / 5 + 32).toFixed(2); // convert Celsius to Fahrenheit with 2 decimals
+        document.getElementById("resultat").innerHTML = "Celsius ===> " + value3;
+    }
+    
+    else if(event.target.id === "celsius"){
+        let value4 = ((number - 32) * 5 / 9).toFixed(2); // convert Fahrenheit to Celsius with 2 decimals
+        document.getElementById("resultat").innerHTML = "Fahrenheit ===> " + value4;
     }
 
-    else if (event.target.id === "celsius") {
-        let value4 = ((number - 32) * 5 / 9).toFixed(2); // conversion from Fahrenheit to Celsius with 2 decimal places
-        document.getElementById("result").innerHTML = "Fahrenheit ===> " + value4;
-    }
-} // End of Function 1
+} // End of function 1
 
 /*
 -------------------------------------------------------
-Title: Function 2 - Display
-Purpose: Display a title with a changing size
+Title: Function 2 - display	 
+Purpose: Display a title with changing size
 -------------------------------------------------------
 */
 
 function display(event) {
-    let name = document.getElementById("name").value; // Get the name string
-    let level = parseInt(document.getElementById("level").value); // Get the number for title size
+    let name = document.getElementById("nom").value; // Get the name string
+    let level = parseInt(document.getElementById("niveau").value); // Get the number for title size
 
-    if (event.target.id === "title") {
+    if (event.target.id === "titre"){
         if (level >= 1 && level <= 6) {
-            document.getElementById("result2").innerHTML = "<h" + level + ">Hello " + name + "<br>Level = " + level + "</h" + level + ">";
-            document.getElementById("error").innerHTML = ""; // Remove the error text
+        document.getElementById("resultat2").innerHTML = "<h" + level + ">Hello " + name + "<br>Level = " + level + "</h" + level + ">";
+        document.getElementById("erreur").innerHTML =""; // Clear error message
         }
         else {
-            document.getElementById("result2").innerHTML = ""; // Remove the result text
-            document.getElementById("error").innerHTML = "The number must be between 1 and 6!!!";
+        document.getElementById("resultat2").innerHTML =""; // Clear result text
+        document.getElementById("erreur").innerHTML = "The number must be between 1 and 6!!!";
         }
     }
-} // End of Function 2
+} // End of function 2
 
 /*
 -------------------------------------------------------
-Title: Function 3 - Menu
+Title: Function 3 - menu	 
 Purpose: Display a dropdown menu
 -------------------------------------------------------
 */
-$(document).ready(function () {
+$(document).ready(function(){
 
     // Function to display appetizers, main courses, desserts, drinks
-    $(".menu-item").click(function () {
-        let $submenu = $(this).next(".submenu"); // Clicking on the menu-item will get the next submenu
-        $(".submenu").not($submenu).slideUp(500, "linear"); // Close other submenus that are not clicked
-        $submenu.slideToggle(500, "linear"); // Toggle the clicked submenu
+    $(".menu-item").click(function(){
+        let $submenu = $(this).next(".submenu"); // when clicking on menu-item, get the next submenu
+        $(".submenu").not($submenu).slideUp(500, "linear"); // Close submenus that are not clicked
+        $submenu.slideToggle(500, "linear"); // Toggle clicked submenu    
     });
 
-    // Variables to store prices
+    // Variables to track prices
     let appetizerPrice = 0;
     let mainCoursePrice = 0;
     let dessertPrice = 0;
     let drinkPrice = 0;
     let total = 0;
 
-    // Variables for the selector
+    // Variables to hold selected items
     let selection = {
         appetizer: null,
         mainCourse: null,
@@ -83,7 +84,7 @@ $(document).ready(function () {
         drink: null,
     };
 
-    // Prices for items
+    // Price variables
     let escargotPrice = 8;
     let saladPrice = 5;
     let caesarSaladPrice = 6;
@@ -94,31 +95,31 @@ $(document).ready(function () {
     let cakePrice = 7;
     let puddingPrice = 5;
     let coffeePrice = 3;
-    let softDrinkPrice = 2;
+    let sodaPrice = 2;
+    
+    // Function to display images with their price
+    $(".submenu li").click(function(){
+        let choice = $(this).text(); // target only the submenu
 
-    // Function to display the images with their prices
-    $(".submenu li").click(function () {
-        let choice = $(this).text(); // Targets the submenu only
-
-        switch (choice) {
+        switch(choice) {
             case "Escargot":
-                if (selection.appetizer === "Escargot") { // Deselect if the selector has an attribute
-                    total -= appetizerPrice; // Subtract the price from the total
-                    appetizerPrice = 0; // Reset the price
-                    selection.appetizer = null; // Reset the selector
-                    $("#image1").html(""); // Remove the image
+                if (selection.appetizer ==="Escargot"){ 
+                    total -= appetizerPrice; 
+                    appetizerPrice = 0; 
+                    selection.appetizer = null; 
+                    $("#image1").html(""); 
                 }
-                else { // Select if the selector is empty
-                    total -= appetizerPrice; // Subtract the old item price from total
-                    appetizerPrice = escargotPrice; // Add the price of the item to the variable
-                    total += appetizerPrice; // Add the price to total
-                    selection.appetizer = "Escargot"; // Add an attribute to the selector
-                    $("#image1").html('<img src="images/escargot.jpg" alt="escargot">' + escargotPrice + '$'); // Display image with its price
+                else { 
+                    total -= appetizerPrice; 
+                    appetizerPrice = escargotPrice; 
+                    total += appetizerPrice; 
+                    selection.appetizer = "Escargot"; 
+                    $("#image1").html('<img src="images/escargot.jpg" alt="escargot">' + escargotPrice + '$'); 
                 }
                 break;
 
             case "Green Salad":
-                if (selection.appetizer === "Green Salad") {
+                if (selection.appetizer ==="Green Salad"){
                     total -= appetizerPrice;
                     appetizerPrice = 0;
                     selection.appetizer = null;
@@ -129,12 +130,12 @@ $(document).ready(function () {
                     appetizerPrice = saladPrice;
                     total += appetizerPrice;
                     selection.appetizer = "Green Salad";
-                    $("#image1").html('<img src="images/salade.jpg" alt="green salad">' + saladPrice + '$');
+                    $("#image1").html('<img src="images/salade.jpg" alt="green salad">'+ saladPrice + '$'); 
                 }
-                break;
+            break;
 
             case "Caesar Salad":
-                if (selection.appetizer === "Caesar Salad") {
+                if (selection.appetizer ==="Caesar Salad"){
                     total -= appetizerPrice;
                     appetizerPrice = 0;
                     selection.appetizer = null;
@@ -145,12 +146,12 @@ $(document).ready(function () {
                     appetizerPrice = caesarSaladPrice;
                     total += appetizerPrice;
                     selection.appetizer = "Caesar Salad";
-                    $("#image1").html('<img src="images/caesar.jpg" alt="caesar salad">' + caesarSaladPrice + '$');
+                    $("#image1").html('<img src="images/salade2.jpg" alt="caesar salad">'+ caesarSaladPrice + '$'); 
                 }
-                break;
+            break;            
 
             case "Spaghetti":
-                if (selection.mainCourse === "Spaghetti") {
+                if (selection.mainCourse ==="Spaghetti"){
                     total -= mainCoursePrice;
                     mainCoursePrice = 0;
                     selection.mainCourse = null;
@@ -161,12 +162,12 @@ $(document).ready(function () {
                     mainCoursePrice = spaghettiPrice;
                     total += mainCoursePrice;
                     selection.mainCourse = "Spaghetti";
-                    $("#image2").html('<img src="images/spaghetti.jpg" alt="spaghetti">' + spaghettiPrice + '$');
+                    $("#image2").html('<img src="images/spaghetti.jpg" alt="spaghetti">'+ spaghettiPrice + '$'); 
                 }
-                break;
+            break;
 
             case "Steak":
-                if (selection.mainCourse === "Steak") {
+                if (selection.mainCourse ==="Steak"){
                     total -= mainCoursePrice;
                     mainCoursePrice = 0;
                     selection.mainCourse = null;
@@ -177,12 +178,12 @@ $(document).ready(function () {
                     mainCoursePrice = steakPrice;
                     total += mainCoursePrice;
                     selection.mainCourse = "Steak";
-                    $("#image2").html('<img src="images/steak.jpg" alt="steak">' + steakPrice + '$');
+                    $("#image2").html('<img src="images/steak.jpg" alt="steak">'+ steakPrice + '$'); 
                 }
                 break;
 
             case "Pizza":
-                if (selection.mainCourse === "Pizza") {
+                if (selection.mainCourse ==="Pizza"){
                     total -= mainCoursePrice;
                     mainCoursePrice = 0;
                     selection.mainCourse = null;
@@ -193,12 +194,12 @@ $(document).ready(function () {
                     mainCoursePrice = pizzaPrice;
                     total += mainCoursePrice;
                     selection.mainCourse = "Pizza";
-                    $("#image2").html('<img src="images/pizza.jpg" alt="pizza">' + pizzaPrice + '$');
+                    $("#image2").html('<img src="images/pizza.jpg" alt="pizza">'+ pizzaPrice + '$');
                 }
-                break;
+            break;
 
             case "Ice Cream":
-                if (selection.dessert === "Ice Cream") {
+                if (selection.dessert ==="Ice Cream"){
                     total -= dessertPrice;
                     dessertPrice = 0;
                     selection.dessert = null;
@@ -209,12 +210,12 @@ $(document).ready(function () {
                     dessertPrice = iceCreamPrice;
                     total += dessertPrice;
                     selection.dessert = "Ice Cream";
-                    $("#image3").html('<img src="images/icecream.jpg" alt="ice cream">' + iceCreamPrice + '$');
+                    $("#image3").html('<img src="images/cremeglacee.jpg" alt="ice cream">'+ iceCreamPrice + '$');
                 }
                 break;
-
+                
             case "Cake":
-                if (selection.dessert === "Cake") {
+                if (selection.dessert ==="Cake"){
                     total -= dessertPrice;
                     dessertPrice = 0;
                     selection.dessert = null;
@@ -225,12 +226,12 @@ $(document).ready(function () {
                     dessertPrice = cakePrice;
                     total += dessertPrice;
                     selection.dessert = "Cake";
-                    $("#image3").html('<img src="images/cake.jpg" alt="cake">' + cakePrice + '$');
+                    $("#image3").html('<img src="images/gateau.jpg" alt="cake">'+ cakePrice + '$'); 
                 }
                 break;
 
             case "Pudding":
-                if (selection.dessert === "Pudding") {
+                if (selection.dessert ==="Pudding"){
                     total -= dessertPrice;
                     dessertPrice = 0;
                     selection.dessert = null;
@@ -241,12 +242,12 @@ $(document).ready(function () {
                     dessertPrice = puddingPrice;
                     total += dessertPrice;
                     selection.dessert = "Pudding";
-                    $("#image3").html('<img src="images/pudding.jpg" alt="pudding">' + puddingPrice + '$');
+                    $("#image3").html('<img src="images/pouding.jpg" alt="pudding">'+ puddingPrice + '$');
                 }
-                break;
+            break;
 
-            case "Coffee":
-                if (selection.drink === "Coffee") {
+            case "Coffee/Tea":
+                if (selection.drink ==="Coffee/Tea"){
                     total -= drinkPrice;
                     drinkPrice = 0;
                     selection.drink = null;
@@ -256,13 +257,13 @@ $(document).ready(function () {
                     total -= drinkPrice;
                     drinkPrice = coffeePrice;
                     total += drinkPrice;
-                    selection.drink = "Coffee";
-                    $("#image4").html('<img src="images/cafe.jpg" alt="coffee">' + coffeePrice + '$');
+                    selection.drink = "Coffee/Tea";
+                    $("#image4").html('<img src="images/coffee.jpg" alt="coffee">'+ coffeePrice + '$');
                 }
                 break;
 
-            case "Soft Drink":
-                if (selection.drink === "Soft Drink") {
+            case "Soda":
+                if (selection.drink ==="Soda"){
                     total -= drinkPrice;
                     drinkPrice = 0;
                     selection.drink = null;
@@ -270,13 +271,13 @@ $(document).ready(function () {
                 }
                 else {
                     total -= drinkPrice;
-                    drinkPrice = softDrinkPrice;
+                    drinkPrice = sodaPrice;
                     total += drinkPrice;
-                    selection.drink = "Soft Drink";
-                    $("#image4").html('<img src="images/boisson.jpg" alt="soft drink">' + softDrinkPrice + '$');
+                    selection.drink = "Soda";
+                    $("#image4").html('<img src="images/boisson.jpg" alt="soda">'+ sodaPrice + '$');
                 }
                 break;
-        }
-        $("#total").text(total + "$"); // Update the total price
+        } // End of switch
+        document.getElementById("resultat3").innerHTML = "Total = " + total + "$"; // Update total
     });
-});
+}); 
